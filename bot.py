be0aa902294c696957ac27898e2277d93c43f731
Wackery Smackery <zapcontact000@gmail.com>
2018-11-06 16:49:01 -0800

    lower limit

--- bot.py
+++ bot.py
@@ -66,8 +66,8 @@ async def search(*, spellLook: str):
     if matched == []:
         await bot.say("No results!")
     else:
-        if len(matched) > 100:
-            await bot.say("More then 100 results! ({}) Narrow your search and try again.".format(len(matched)))
+        if len(matched) > 20:
+            await bot.say("More then 20 results! ({}) Narrow your search and try again.".format(len(matched)))
             return
         emb = discord.Embed(title = "Search Results", type = "rich", description = "Search results for {}".format(spellLook),color = color)
         out = ""
