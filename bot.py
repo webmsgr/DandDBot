9795b6274c059b561e479a258a426ab548f2418e
Wackery Smackery <zapcontact000@gmail.com>
2018-11-06 16:20:25 -0800

    fx=x

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ async def spell(*, spell: str):
         await bot.say("That spell does not exist")
         return
     thespell = spelll[spelli]
-    emb = discord.Embed(title = spelli,type = "rich", description = thespell.pop("desc"))
+    emb = discord.Embed(title = spelli,type = "rich", description = thespell.pop("Desc"))
     for thing in thespell:
         emb.add_field(name=thing,value=thespell[thing])
     await bot.say("",embed=emb)
